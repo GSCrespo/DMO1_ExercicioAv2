096ef9e6dca43ad47d883a3d68a67bb5325b3faf
Guilherme Crespo AQ3022889 <guilherme.crespo@aluno.ifsp.edu.br>
2025-01-25 18:03:32 -0300

    Commit inicial do aplicativo, contendo também a primeira versão do layout da MainActivity

---
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.edu.ifsp.dmo1.pesquisaopiniao.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo1.pesquisaopiniao.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
