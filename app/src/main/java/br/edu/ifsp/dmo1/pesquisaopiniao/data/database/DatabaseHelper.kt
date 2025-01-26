cd8a60c406f4c2df77fa965ab1b0a1328ebaaeea
Guilherme Crespo AQ3022889 <guilherme.crespo@aluno.ifsp.edu.br>
2025-01-26 17:57:55 -0300

    Adicionado a classe DatabaseHelper ao projeto, juntamente com uma pequena modificação na activity_participate_search

---
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/data/database/DatabaseHelper.kt
@@ -0,0 +1,53 @@
+package br.edu.ifsp.dmo1.pesquisaopiniao.data.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper (context: Context) : SQLiteOpenHelper(context, DATABASE_KEYS.DATABASE_NAME,null,DATABASE_KEYS.DATABASE_VERSION) {
+
+    object DATABASE_KEYS{
+        const val DATABASE_NAME = "PesquisaVotos_databse.db"
+        const val DATABASE_VERSION = 1
+        const val TABLE_ALUNO = "tb_aluno"
+        const val TABLE_VOTO = "tb_voto"
+
+        const val COLUMN_ALUNO_PRONTUARIO = "prontuario"
+        const val COLUMN_ALUNO_NAME = "nome"
+
+        const val COLUMN_VOTO_ID = "id"
+        const val COLUMN_VOTO_CODIGO = "codigo"
+
+        // chave estrangeira pra relacionar 1 voto com apenas 1 aluno
+        const val COLUMN_VOTO_PRONTUARIO = "voto_prontuario"
+
+    }
+
+    private companion object {
+
+        const val CREATE_TABLE_ALUNO = "CREATE TABLE ${DATABASE_KEYS.TABLE_ALUNO} " +
+                "(" + "${DATABASE_KEYS.COLUMN_ALUNO_PRONTUARIO} TEXT PRIMARY KEY NOT NULL," +
+                "${DATABASE_KEYS.COLUMN_ALUNO_NAME})"
+
+        const val CREATE_TABLE_VOTO = "CREATE TABLE ${DATABASE_KEYS.TABLE_VOTO} " +
+                "(" + "${DATABASE_KEYS.COLUMN_VOTO_ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "${DATABASE_KEYS.COLUMN_VOTO_CODIGO}," +
+                "${DATABASE_KEYS.COLUMN_VOTO_PRONTUARIO} TEXT NOT NULL," +
+                "FOREIGN KEY (${DATABASE_KEYS.COLUMN_VOTO_PRONTUARIO})" +
+                "REFERENCES ${DATABASE_KEYS.TABLE_ALUNO}(${DATABASE_KEYS.COLUMN_VOTO_PRONTUARIO}) " +
+                "ON DELETE CASCADE ON UPDATE CASCADE)"
+
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(CREATE_TABLE_ALUNO)
+        db.execSQL(CREATE_TABLE_VOTO)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+
+        // por enquanto nao ha necessidade de outra versao no banco, deixarei nao implementado
+    }
+
+
+}
