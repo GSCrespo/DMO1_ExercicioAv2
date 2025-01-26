eefc7bc8fa79baa58dabeb7926f9120276ae2a6e
Guilherme Crespo AQ3022889 <guilherme.crespo@aluno.ifsp.edu.br>
2025-01-26 02:58:57 -0300

    Adicionando a activity de participação na pesquisa(commit inicial apenas)

---
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/ui/details/participateSearch.kt
@@ -0,0 +1,19 @@
+package br.edu.ifsp.dmo1.pesquisaopiniao.ui.details
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.dmo1.pesquisaopiniao.R
+
+class participateSearch : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_participate_search)
+
+    }
+}
--- app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package br.edu.ifsp.dmo1.pesquisaopiniao.ui
+package br.edu.ifsp.dmo1.pesquisaopiniao.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
