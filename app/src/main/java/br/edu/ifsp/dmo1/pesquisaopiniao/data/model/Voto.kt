d053f9d0c2c42982e6bb06e88610404504e5b347
Guilherme Crespo AQ3022889 <guilherme.crespo@aluno.ifsp.edu.br>
2025-01-27 14:10:58 -0300

    lógica de geração de Strings unicas implementada na classe de dados Voto

--- app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/data/model/Voto.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/data/model/Voto.kt
@@ -1,6 +1,22 @@
 package br.edu.ifsp.dmo1.pesquisaopiniao.data.model
 
-class Voto (val codigo: String, val opcao: String) {
+import java.util.UUID
+
+class Voto (val codigo : String = gerarStringAleatoria(), val opcao: String) {
 
 
+    // como a função vai ser usada so pra gerar o codigo do voto, se torna + adequado
+    // deixar a funçao como estatica (seguindo a logica do contrato q esta no DataHelper )
+    private companion object {
+
+        fun gerarStringAleatoria(tamanho : Int = 10) : String{
+            // esse metodo utiliza UUID para gerar 32 caracteres hexadecimais separados por hifen
+            // no caso aqui vou limitar a 10 conforme documento de requisitos
+
+            val uuid = UUID.randomUUID().toString().replace("-","")
+            // usando o replace para retirar os hifens entre os caracteres
+            return  uuid.take(tamanho)
+        }
+
+    }
 }
